6267eb9f3ec703c864fbc10db00aa2aed176fb5f
Aman Bhargava <amanb2000@gmail.com>
2022-06-14 03:13:05 -0400

    Remove old non-3D neural net rendering code

--- src/net_visualizations.jl
+++ src/net_visualizations.jl
@@ -85,58 +85,6 @@ function draw_real_net(real_node_pts, meta; circle_color="black",
 end
 
 
-function draw_net(node_perc_list, min_x, max_x, min_y, max_y; alpha=1.0)
-
-	# lines
-	setcolor((0.3, 0.3, 0.3, alpha))
-	dolines(node_poss)
-
-
-	# circles
-	setcolor((0.1, 0.1, 0.1, alpha))
-	docircles(node_poss)
-
-	return node_poss
-end
-
-function dolines(node_poss, line_weight=1)
-	setline(line_weight)
-	for l = 1:length(node_poss)-1
-		for p1 in node_poss[l]
-			for p2 in node_poss[l+1]
-				line(p1,p2, :stroke)
-			end
-		end
-	end
-end
-
-function docircles(node_poss, circle_size=20)
-	for layer in node_poss
-		for p in layer 
-			# println(p)
-			circle(p, circle_size, :fill)
-		end
-	end
-end
-
-
-
-## Setting up the drawing
-# Drawing(2048, 2048, "tests/06_networks.pdf")
-# origin() # setting zero point to center.
-# # the following two commands are 
-# rad = 300
-# abs_offset = -300
-# for ofst = 0:10:100
-# 	node_poss = get_even_net_points(layer_sizes, min_x, max_x, min_y, max_y)
-# 	draw_net(node_poss, -rad+ofst+abs_offset, rad+(ofst*5)+abs_offset, -rad+ofst+abs_offset, rad+(ofst*5)+abs_offset; alpha= (ofst/100.)^2)
-# end
-
-
-
-# finish()	
-
-
 function flatten(net_points)
 	retval = []
 	for row in net_points
